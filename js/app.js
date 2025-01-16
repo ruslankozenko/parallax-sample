2e953828a01752535f0464d782cfbbacb8b7816e
Ruslan Kozenko <kozenko.r@seznam.cz>
2025-01-16 12:02:42 +0100

    parallax off

--- js/app.js
+++ js/app.js
@@ -14,3 +14,25 @@ const sliderMain = new Swiper('.slider_main' , {
         }
     }
 })
+
+const sliderBg = new Swiper('.slider_bg', {
+    
+    centeredSlides: true,
+    parallax: true,
+    spaceBetween: 60,
+    slidesPerView: 3.5,
+})
+
+sliderMain.controller.control = sliderBg
+
+document.querySelectorAll('.slider_item').forEach(item => {
+    item.addEventListener('click', event => {
+        item.classList.toggle('opened')
+    } )
+})
+
+let desc = document.querySelector('.description')
+
+sliderMain.on('slideChange', e => {
+    sliderMain.activeIndex > 0 ? desc.classList.add('hidden') : desc.classList.remove('hidden')
+})
